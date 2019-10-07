86410961c22fcdafe1f9f05b5b809db32fc48b63
HUNcamper <selimnahimi@gmail.com>
2019-10-07 23:17:00 +0200

    Updated DocFX to exclude SDL2, hello world window using SDL2

--- tower-blocks/tower-blocks/src/Main.cs
+++ tower-blocks/tower-blocks/src/Main.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SDL2;
+
 
 namespace tower_blocks
 {
@@ -17,8 +19,35 @@ namespace tower_blocks
         /// <param name="args">Command line arguments</param>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
+            SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
+
+            var window = IntPtr.Zero;
+            window = SDL.SDL_CreateWindow("Hello World",
+                SDL.SDL_WINDOWPOS_CENTERED,
+                SDL.SDL_WINDOWPOS_CENTERED,
+                1028,
+                800,
+                SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE
+            );
+
+            //SDL.SDL_Delay(1000);
+
+            SDL.SDL_Event e;
+            bool quit = false;
+            while (!quit)
+            {
+                while (SDL.SDL_PollEvent(out e) != 0)
+                {
+                    switch (e.type)
+                    {
+                        case SDL.SDL_EventType.SDL_QUIT:
+                            quit = true;
+                            break;
+                    }
+                }
+            }
+
+            SDL.SDL_DestroyWindow(window);
         }
     }
 }
