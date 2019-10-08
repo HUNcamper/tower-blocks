544f12dd3e4ce6bd2a7980aa0fa6272adba58a90
HUNcamper <selimnahimi@gmail.com>
2019-10-08 11:23:13 +0200

    updated SDL handler

--- tower-blocks/tower-blocks/src/Main.cs
+++ tower-blocks/tower-blocks/src/Main.cs
@@ -22,18 +22,13 @@ namespace tower_blocks
             SDL_Handler sdl_handler = new SDL_Handler();
 
             sdl_handler.CreateWindow("Hello World", 1024, 800);
-            sdl_handler.CreateWindow("Hello World", 1024, 800);
-            sdl_handler.CreateWindow("Hello World", 1024, 800);
-            sdl_handler.CreateWindow("Hello World", 1024, 800);
-            sdl_handler.CreateWindow("Hello World", 1024, 800);
+            sdl_handler.CreateWindow("Hello World", 100, 500);
 
             //SDL.SDL_Delay(1000);
 
-            SDL.SDL_Event e;
-            bool quit = false;
-            while (!quit)
+            while (!sdl_handler.quit)
             {
-                while (SDL.SDL_PollEvent(out e) != 0)
+                /*while (SDL.SDL_PollEvent(out e) != 0)
                 {
                     switch (e.type)
                     {
@@ -41,7 +36,9 @@ namespace tower_blocks
                             quit = true;
                             break;
                     }
-                }
+                }*/
+
+                sdl_handler.Update();
             }
 
             //SDL.SDL_DestroyWindow(window);
--- tower-blocks/tower-blocks/src/other/SDL_Handler.cs
+++ tower-blocks/tower-blocks/src/other/SDL_Handler.cs
@@ -10,14 +10,33 @@ namespace tower_blocks
     /// <summary>
     /// Handles certain SDL functions
     /// </summary>
-    class SDL_Handler
+    public class SDL_Handler
     {
         private List<IntPtr> window_list = new List<IntPtr>();
+
+        /// <summary>
+        /// Is the SDL handler done
+        /// </summary>
+        public bool quit { get; set; }
 
         public SDL_Handler()
         {
             // Initialize SDL2
             SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
+            quit = false;
+        }
+
+        /// <summary>
+        /// Update function, has to be called in a loop
+        /// </summary>
+        public void Update()
+        {
+            SDL.SDL_Event e;
+            if (SDL.SDL_PollEvent(out e) != 0)
+            {
+                HandleEvents(e);
+                HandleWindowEvents(e);
+            }
         }
 
         /// <summary>
@@ -28,8 +47,9 @@ namespace tower_blocks
         /// <param name="w_height">Window height</param>
         /// <param name="w_h_windowpos">Window's horizontal position</param>
         /// <param name="w_v_windowpos">Window's vertical position</param>
-        /// <param name="w_windowflags">Window's SDL.SDL_WindowFlags flags</param>
-        public void CreateWindow (string w_title, int w_width, int w_height, int w_h_windowpos = SDL.SDL_WINDOWPOS_CENTERED, int w_v_windowpos = SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WindowFlags w_windowflags = SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE)
+        /// <param name="w_windowflags">Window's flags</param>
+        /// <returns>Created window</returns>
+        public IntPtr CreateWindow(string w_title, int w_width, int w_height, int w_h_windowpos = SDL.SDL_WINDOWPOS_CENTERED, int w_v_windowpos = SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WindowFlags w_windowflags = SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE)
         {
             IntPtr window = IntPtr.Zero;
 
@@ -42,6 +62,58 @@ namespace tower_blocks
             );
 
             window_list.Add(window);
+
+            return window;
+        }
+
+        /// <summary>
+        /// Destroy a given window
+        /// </summary>
+        /// <param name="window">window's pointer</param>
+        public void DestroyWindow(IntPtr window)
+        {
+            SDL.SDL_DestroyWindow(window);
+            window_list.Remove(window);
+        }
+
+        /// <summary>
+        /// Destroy a given window
+        /// </summary>
+        /// <param name="windowID">window's ID</param>
+        public void DestroyWindow(uint windowID)
+        {
+            IntPtr window = SDL.SDL_GetWindowFromID(windowID);
+            DestroyWindow(window);
+        }
+
+        /// <summary>
+        /// Handles events
+        /// </summary>
+        public void HandleEvents(SDL.SDL_Event e)
+        {
+            switch (e.type)
+            {
+                case SDL.SDL_EventType.SDL_QUIT:
+                    quit = true;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handles window events
+        /// </summary>
+        public void HandleWindowEvents(SDL.SDL_Event e)
+        {
+            if (e.type == SDL.SDL_EventType.SDL_WINDOWEVENT)
+            {
+                switch (e.window.windowEvent)
+                {
+                    // Close window when X is pressed
+                    case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_CLOSE:
+                        DestroyWindow(e.window.windowID);
+                        break;
+                }
+            }
         }
     }
 }
